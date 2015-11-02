993a63a62944c5999309f99327f8515c04a32b01
Damir Cohadarevic <damir.cohadarevic@sbgenomics.com>
2015-11-02 20:46:17 +0100

    simplicity

--- ProjectEuler/LargestPrimeFactor/LargestPrimeFactor.java
+++ ProjectEuler/LargestPrimeFactor/LargestPrimeFactor.java
@@ -4,36 +4,24 @@ import java.io.*;
 /* Mighty Cohadar */
 public class LargestPrimeFactor {
 
-	static int[] sieveOfEratosthenes(int n) {
-		int[] P = new int[n];
-		for (int i = 0; i < P.length; i++) { P[i] = i; }
-		for (int p = 2; p * p < n; p++) {
-			while (P[p] == 0) { p++; }
-			for (int k = p * p; k < P.length; k += p) { P[k] = 0; }
+	static long largestPrime(long n) {
+		long lp = 1;		
+		for (long i = 2; i * i <= n; i++) {
+			while (n % i == 0) {
+				lp = i;
+				n /= i;
+			}
 		}
-		return P;
-	}
-
-	static long largestPrime(int[] P, long n) {
-		long lp = 1;
-		for (int i = 2; i < P.length; i++) {
-			if (P[i] != 0) {
-				while (n % P[i] == 0) {
-					lp = P[i];
-					n /= lp;
-				};
-			}	
-		}
-		return (lp == 1) ? n : Math.max(lp, n); // <---<< this max was hard to find
+		// return Math.max(lp, n);
+		return (n > 1) ? n : lp;
 	}
 
 	public static void main(String[] args) {
-		int[] P = sieveOfEratosthenes(1000000);
 		Scanner scanner = new Scanner(System.in);
 		int t = scanner.nextInt();
 		while (t-->0) {
-			long n = scanner.nextLong();
-			System.out.println(largestPrime(P, n));
+			long l = scanner.nextLong();
+			System.out.println(largestPrime(l));
 		}
 	}
 
