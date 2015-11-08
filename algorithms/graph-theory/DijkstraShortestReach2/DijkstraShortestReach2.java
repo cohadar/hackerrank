cbcd00fd8b177a175c1443f02f9553860108cf3f
Damir Cohadarevic <damir.cohadarevic@sbgenomics.com>
2015-11-08 22:43:50 +0100

    stupid duplicates

--- algorithms/graph-theory/DijkstraShortestReach2/DijkstraShortestReach2.java
+++ algorithms/graph-theory/DijkstraShortestReach2/DijkstraShortestReach2.java
@@ -4,7 +4,19 @@ import java.io.*;
 /* Mighty Cohadar */
 public class DijkstraShortestReach2 {
 
-	static final int PRIME = 1000000007;
+	static final int INFINITY = 1000000007;
+
+	static class Edge {
+		final int b;
+		final int w;
+		Edge(int b, int w) {
+			this.b = b;
+			this.w = w;
+		}
+		public String toString() {
+			return String.format("(b=%d, w=%d)", b, w);
+		}	
+	}	
 
 	static class Vertex implements Comparable<Vertex>{
 		final int a;
@@ -16,21 +28,26 @@ public class DijkstraShortestReach2 {
 		public int compareTo(Vertex that) {
 			return Integer.compare(this.w, that.w);
 		}
+		public String toString() {
+			return String.format("(a=%d, w=%d)", a, w);
+		}			
 	}
 
-	static int[] solve(int n, int[][] M, int s) {
+	static int[] solve(int n, List<List<Edge>> AL, int s) {
 		boolean[] B = new boolean[n];
 		int[] D = new int[n];
-		Arrays.fill(D, PRIME);
+		Arrays.fill(D, INFINITY);
 		D[s] = 0;
 		Queue<Vertex> Q = new PriorityQueue<Vertex>();
 		Q.add(new Vertex(s, 0));
 		while (!Q.isEmpty()) {
 			Vertex v = Q.remove();
-			for (int b = 0; b < n; b++) {
-				if (B[b] || M[v.a][b] == PRIME || v.a == b) continue;
-				D[b] = Math.min(D[b], D[v.a] + M[v.a][b]);
-				Q.add(new Vertex(b, D[b]));
+			System.out.println(v);
+			List<Edge> al = AL.get(v.a);
+			for (Edge e : al) {
+				if (B[e.b]) continue;
+				D[e.b] = Math.min(D[e.b], D[v.a] + e.w);
+				Q.add(new Vertex(e.b, D[e.b]));
 			}
 			B[v.a] = true;
 		}
@@ -46,11 +63,9 @@ public class DijkstraShortestReach2 {
 			int m = scanner.nextInt();
 			assert 2 <= n && n <= 3000;
 			assert 1 <= m && m <= n * (n - 1) / 2;
-			int[][] M = new int[n][n];
+			List<List<Edge>> AL = new ArrayList<>();
 			for (int i = 0; i < n; i++) {
-				for (int j = 0; j < n; j++) {
-					M[i][j] = PRIME;
-				}
+				AL.add(new ArrayList<Edge>());
 			}
 			for (int i = 0; i < m; i++) {
 				int x = scanner.nextInt() - 1;
@@ -59,12 +74,13 @@ public class DijkstraShortestReach2 {
 				assert 0 <= x && x < n;
 				assert 0 <= y && y < n;
 				assert x != y;
-				M[x][y] = Math.min(M[x][y], w);
-				M[y][x] = Math.min(M[y][x], w);
+				// handle duplicates
+				AL.get(x).add(new Edge(y, w));
+				AL.get(y).add(new Edge(x, w));
 			}
 			int s = scanner.nextInt() - 1;
 			assert 0 <= s && s < n;
-			int[] distances = solve(n, M, s);
+			int[] distances = solve(n, AL, s);
 			System.out.println(join(distances, ' ', s));
 		}
 	}
@@ -73,7 +89,7 @@ public class DijkstraShortestReach2 {
 		StringBuilder sb = new StringBuilder();
 		for (int i = 0; i < A.length; i++) {
 			if (i != s) {
-				sb.append((A[i] == PRIME) ? -1 : A[i]);
+				sb.append((A[i] == INFINITY) ? -1 : A[i]);
 				sb.append(delimiter);
 			}
 		}
