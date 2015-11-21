ef7d4102cb3850cb4cd9a1f0e2ef71acc71aba68
Damir Cohadarevic <damir.cohadarevic@sbgenomics.com>
2015-11-21 09:02:18 +0100

    last node

--- utils/TreeDisplay/TreeDisplay.java
+++ utils/TreeDisplay/TreeDisplay.java
@@ -5,13 +5,42 @@ import java.io.*;
 public class TreeDisplay {
 
 	private final List<List<Integer>> G;
+	private final Deque<Boolean> LC = new ArrayDeque<>();
+	private final boolean[] V;
 	
 	public TreeDisplay(List<List<Integer>> G) {
 		this.G = G;
+		this.V = new boolean[G.size()];
 	}
 
-	void print(int start) {
-		/***/
+	void print_line(int a) {
+		Iterator<Boolean> i = LC.descendingIterator();
+		int s = LC.size();
+		int c = 0;
+		while (i.hasNext()) {
+			c++;
+			if (c == s) {
+				System.out.print((i.next()) ? "`-- " : "|-- ");	
+			} else {
+				System.out.print((i.next()) ? "    " : "|   ");
+			}
+		}
+		System.out.printf("(%d)\n", a + 1);
+	}
+
+	void print(int a) {
+		V[a] = true;
+		print_line(a);
+		int s = G.get(a).size();
+		int c = 0;
+		for (int b : G.get(a)) {
+			c++;
+			LC.push(c == s);
+			if (!V[b]) {
+				print(b);
+			}
+			LC.pop();
+		}
 	}
 
 	public static void main(String[] args) {
@@ -20,8 +49,15 @@ public class TreeDisplay {
 		int m = scanner.nextInt();
 		assert n == m + 1;
 		List<List<Integer>> G = scanGraph(scanner, n, m);
+		sortChildren(G);
 		TreeDisplay o = new TreeDisplay(G);
 		o.print(0);
+	}
+
+	static void sortChildren(List<List<Integer>> G) {
+		for (int i = 0; i < G.size(); i++) {
+			Collections.sort(G.get(i));
+		}
 	}
 
 	static List<List<Integer>> scanGraph(Scanner scanner, int n, int m) {
