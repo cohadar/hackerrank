cc77910073319ce17d22085effaa65608a0d2a27
Damir Cohadarevic <damir.cohadarevic@sbgenomics.com>
2015-11-22 16:39:27 +0100

    Floyd-Warshall

--- algorithms/graph-theory/FloydCityOfBlindingLights/FloydCityOfBlindingLights.java
+++ algorithms/graph-theory/FloydCityOfBlindingLights/FloydCityOfBlindingLights.java
@@ -4,6 +4,31 @@ import java.io.*;
 /* Mighty Cohadar */
 public class FloydCityOfBlindingLights {
 
+	static final int INF = (int)1e9 + 7;
+
+	static int[][] solve(int[][] G) {
+		int n = G.length;
+		int[][] D = new int[n][n];
+		for (int a = 0; a < n; a++) {
+			for (int b = 0; b < n; b++) {
+				if (a == b) {
+					D[a][b] = 0;	
+				} else {
+					D[a][b] = (G[a][b] > 0) ? G[a][b] : INF;
+				}
+			}
+		}
+		for (int c = 0; c < n; c++) {
+			for (int a = 0; a < n; a++) {
+				for (int b = 0; b < n; b++) {
+					int acb = D[a][c] + D[c][b];
+					D[a][b] = Math.min(D[a][b], D[a][c] + D[c][b]);
+				}
+			}
+		}
+		return D;
+	}
+
 	public static void main(String[] args) {
 		Scanner scanner = new Scanner(System.in);
 		int n = scanner.nextInt();
@@ -11,12 +36,13 @@ public class FloydCityOfBlindingLights {
 		int m = scanner.nextInt();
 		assert 1 <= m && m <= n * (n - 1) / 2 : "out of range, m: " + m;
 		int[][] G = scanGraph(scanner, n, m);
+		int[][] D = solve(G);
 		int q = scanner.nextInt();
-		assert 1 <= q && q <= 10e5 : "out of range, q: " + q;
+		assert 1 <= q && q <= 1e5 : "out of range, q: " + q;
 		for (int i = 0; i < q; i++) {
-			int a = scanner.nextInt();
-			int b = scanner.nextInt();
-			System.out.println(-1);
+			int a = scanner.nextInt() - 1;
+			int b = scanner.nextInt() - 1;
+			System.out.println((D[a][b] < INF) ? D[a][b] : -1);
 		}
 	}
 
