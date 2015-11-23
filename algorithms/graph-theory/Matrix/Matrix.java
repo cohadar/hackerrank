69ba50d4aa9cf7aed9d2283cd472f9a342353eae
Damir Cohadarevic <damir.cohadarevic@sbgenomics.com>
2015-11-23 22:18:49 +0100

    error in Matrix

---
+++ algorithms/graph-theory/Matrix/Matrix.java
@@ -0,0 +1,137 @@
+import java.util.*;
+import java.io.*;
+
+/* Mighty Cohadar */
+public class Matrix {
+
+	static class Edge implements Comparable<Edge> {
+		final int a;
+		final int b;
+		final int z;
+		boolean ma;
+		boolean mb;
+		boolean destroyed = false;
+		Edge(int a, int b, int z) {
+			this.a = a;
+			this.b = b;
+			this.z = z;
+		}
+		public String toString() {
+			return String.format("(a=%d, b=%d, z=%d)", a, b, z);
+		}	
+		public int compareTo(Edge that) {
+			return Integer.compare(this.z, that.z);
+		}
+	}
+
+	private final List<List<Edge>> G;
+	private final int[] K;
+	
+	public Matrix(List<List<Edge>> G, int[] K) {
+		Arrays.sort(K);
+		debug(G);
+		debug(K);
+		this.G = G;
+		this.K = K;
+	}
+
+	boolean leadsToMachines(int start, Edge e) {
+		boolean mb = (start == e.a) ? e.mb : e.ma;
+		if  (mb) {
+			return true;
+		}
+		int b = (start == e.a) ? e.b : e.a;
+		for (Edge f : G.get(b)) {
+			if (!f.destroyed) {
+				f.destroyed = true;
+				if (leadsToMachines(f.a, f)) {
+					f.destroyed = false;
+					return true;
+				}
+				if (leadsToMachines(f.b, f)) {
+					f.destroyed = false;
+					return true;
+				}
+				f.destroyed = false;
+			}
+		}
+		return false;
+	}
+
+	void locateMachines() {
+		for (List<Edge> le : G) {
+			for (Edge e : le) {
+				e.ma = Arrays.binarySearch(K, e.a) >= 0;
+				e.mb = Arrays.binarySearch(K, e.b) >= 0;
+			}
+		}
+	}
+
+	long solve() {
+		locateMachines();
+		PriorityQueue<Edge> Q = new PriorityQueue<>();
+		for (int k : K) {
+			for (Edge e : G.get(k)) {
+				Q.add(e);
+			}
+		}
+		long time = 0;
+		while (!Q.isEmpty()) {
+			Edge e = Q.remove();
+			if (!e.destroyed) {
+				e.destroyed = true;
+				if (leadsToMachines(e.a, e) && leadsToMachines(e.b, e)) {
+					time += e.z;
+				} else {
+					e.destroyed = false;
+				}
+			}
+		}
+		return time;
+	}
+
+	public static void main(String[] args) {
+		Scanner scanner = new Scanner(System.in);
+		int n = scanner.nextInt();
+		int k = scanner.nextInt();
+		assert 2 <= n && n <= 1e5 : "out of range, n: " + n;
+		assert 2 <= k && k <= n : "out of range, k: " + k;
+		List<List<Edge>> G = scanGraph(scanner, n, n - 1);
+		int[] K = scanArray(scanner, k);
+		Matrix o = new Matrix(G, K);
+		System.out.println(o.solve());
+	}
+
+	static List<List<Edge>> scanGraph(Scanner scanner, int n, int m) {
+		List<List<Edge>> G = new ArrayList<>();
+		for (int i = 0; i < n; i++) {
+			G.add(new ArrayList<Edge>());
+		}
+		for (int i = 0; i < m; i++) {
+			int a = scanner.nextInt();
+			int b = scanner.nextInt();
+			int z = scanner.nextInt();
+			assert 1 <= z && z <= 1e6 : "out of range, z: " + z;
+			Edge e = new Edge(a, b, z);
+			G.get(a).add(e);
+			G.get(b).add(e);
+		}		
+		return G;
+	}
+
+	static int[] scanArray(Scanner scanner, int n) {
+		int[] A = new int[n];
+		for (int i = 0; i < n; i++) {
+			A[i] = scanner.nextInt();
+		}
+		return A;
+	}
+
+	static boolean DEBUG = false;
+	
+	static void debug(Object...os) {
+		if (!DEBUG) { return; }
+		System.err.printf("%.65536s\n", Arrays.deepToString(os));
+	}
+}
+
