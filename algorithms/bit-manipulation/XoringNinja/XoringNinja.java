adda4a12f6287a18b13b6da1cfea0fff4bfa588a
Damir Cohadarevic <damir.cohadarevic@sbgenomics.com>
2015-12-15 11:19:51 +0100

    crazy binomials

--- algorithms/bit-manipulation/XoringNinja/XoringNinja.java
+++ algorithms/bit-manipulation/XoringNinja/XoringNinja.java
@@ -4,26 +4,68 @@ import java.io.*;
 /* Mighty Cohadar */
 public class XoringNinja {
 
-	static final int PRIME = 1000000007;
+	static final int PRIME = (int)1e9 + 7;
 
-	static int solve(int[] A) {
-		return 3;
+	static long solve(int[] A) {
+		long sum = 0;
+		for (int i = 0; i < 31; i++) {
+			int ones = 0;
+			int zeros = 0;
+			int MASK = 1 << i;
+			for (int ia = 0; ia < A.length; ia++) {
+				if ((A[ia] & MASK) == 0) {
+					zeros++;
+				} else {
+					ones++;
+				}
+			}
+			sum += (njak(zeros, ones) << i);
+			sum %= PRIME;
+		}
+		return sum;
 	}
 
-	static void test() {
-		for (int i = 0; i < 8; i++) {
-			System.out.printf("%d %d %d\n", i, (3 ^ i) + (2 ^ i), (3 + 2) ^ i);
+	static long njak(int zeros, int ones) {
+		// zsum == 2 ** zeros
+		long zsum = 0;
+		long zcumul = 1;
+		for (int i = zeros; i >= 0; i--) {
+			zsum += zcumul;
+			zcumul *= i;
+			zsum %= PRIME;
+			zcumul %= PRIME;
+		}
+		long osum = 0;
+		long ocumul = ones;
+		int j = ones;
+		while (ocumul > 0) {
+			osum += ocumul;
+			osum %= PRIME;
+			ocumul *= (--j);
+			ocumul %= PRIME;
+			ocumul *= (--j);
+			ocumul %= PRIME;
+		}
+		System.out.printf("osum=%d, zsum=%d\n", osum, zsum);
+		return (osum * zsum) % PRIME;
+	}
+
+	static void test_njak() {
+		for (int z = 0; z < 5; z++) {
+			for (int o = 0; o < 5; o++) {
+				System.out.printf("njak(%d, %d) == %d\n", z, o, njak(z, o));
+			}
 		}
 	}
 
 	public static void main(String[] args) {
-		test();
+		test_njak();
 		Scanner scanner = new Scanner(System.in);
 		int t = scanner.nextInt();
 		while (t-->0) {
 			int n = scanner.nextInt();
 			int[] A = scanArray(scanner, n);
-			//System.out.println(solve(A));
+			System.out.println(solve(A));
 		}
 	}
 
