6aececb2b46c056115679394e47d9e2257a3ec88
Damir Cohadarevic <damir.cohadarevic@sbgenomics.com>
2015-12-31 11:05:17 +0100

    no time

---
+++ algorithms/bit-manipulation/Cipher/Cipher.java
@@ -0,0 +1,13 @@
+import java.util.*;
+import java.io.*;
+
+/* Mighty Cohadar */
+public class Cipher {
+
+	public static void main(String[] args) {
+		Scanner scanner = new Scanner(System.in);
+
+	}
+
+}
+
