87bd7c705566921818155f668eea8cc3e808cbae
Damir Cohadarevic <damir.cohadarevic@sbgenomics.com>
2016-01-03 12:13:40 +0100

    done Clique

--- algorithms/graph-theory/Clique/Clique.java
+++ algorithms/graph-theory/Clique/Clique.java
@@ -1,68 +1,53 @@
 import java.util.*;
 import java.io.*;
 
-/* Mighty Cohadar */
+/**
+  * @author Mighty Cohadar 
+  * @link 
+  */
 public class Clique {
 
-	// maximum number of m in a graph with no (r + 1) cliques
-	// if m is bigger it has at least an (r + 1) clique
-	static int turan(int n, int r) {
-		long res = n * n;
-		res *= (r - 1);
-		res /= (2 * r);
-		return (int)res;
+	static long turan(int n, int r) {
+		long n2 = (long)n * (long)n;
+		long nmr = n % r;
+		long c = (long)Math.ceil((double)n / (double)(r));
+		long f = (long)Math.floor((double)n / (double)(r));
+		return (n2 - nmr * c * c - (r - nmr) * f * f) / 2;
 	}
 
-	static int solve(int n, int m) {
-		int low = 1;
-		int high = n;
-		int ret = 1;
-		while (low <= high) {
-			if (high - low <= 16) {
-				for (int r = low; r <= high; r++) {
-					int t = turan(n, r);
-					if (t < m) {
-						ret = r + 1;
-					} else {
-						break;
-					}
-				}
-				return ret;
-			}
-			int mid = (low + high) >>> 1;
-			int t = turan(n, mid);
-			if (t < m) {
-				ret = Math.max(ret, mid + 1);
-				low = mid;
+	static int lowerBound(int left, int right, int value) {
+		int n = right;
+		right--;
+		while (left <= right) {
+			int mid = (left + right) >>> 1;
+			int midValue = (int)turan(n, mid);
+			if (midValue >= value) {
+				right = mid - 1;
 			} else {
-				high = mid;
+				left = mid + 1;
 			}
 		}
-		assert false : "grrrrr";
-		return 1;
-	}
+		return left;
+	}	
 
-	static int solve2(int n, int m) {
-		int r = 1;
-		while (turan(n, r) < m) {
-			r++;
-		}
-		return r;
+	static int solve(final int n, int m) {
+		return lowerBound(1, n, m);
 	}
 
 	public static void main(String[] args) {
 		Scanner scanner = new Scanner(System.in);
+		StringBuilder sb = new StringBuilder();
 		int t = scanner.nextInt();
 		assert 1 <= t && t <= 1e5 : "out of range, t: " + t;
-		int max_n = (int)1e4;
 		while (t-->0) {
 			int n = scanner.nextInt();
 			int m = scanner.nextInt();
 			assert 2 <= n && n <= 1e4 : "out of range, n: " + n;
 			assert 1 <= m && m <= n * (n - 1) / 2 : "out of range, m: " + m;
-			System.out.println(solve2(n, m));
+			sb.append(solve(n, m));
+			sb.append('\n');
 		}
+		System.out.println(sb);
 	}
 
 }
-
