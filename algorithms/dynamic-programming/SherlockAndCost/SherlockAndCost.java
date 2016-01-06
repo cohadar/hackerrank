dfc944cf0cf6567a6f906ebb8bd53f273bb57a3b
Damir Cohadarevic <damir.cohadarevic@sbgenomics.com>
2016-01-06 19:57:08 +0100

    done sherlock cost

--- algorithms/dynamic-programming/SherlockAndCost/SherlockAndCost.java
+++ algorithms/dynamic-programming/SherlockAndCost/SherlockAndCost.java
@@ -6,36 +6,35 @@ import java.io.*;
   */
 public class SherlockAndCost {
 
-	static final int MAX_B = 100;
+	static final int MIN = 0;
+	static final int MAX = 1;
 
 	private final int[] B;
 	private final int[][] D;
+	private final int n;
 	
 	public SherlockAndCost(int[] B) {
 		this.B = B;
-		this.D = new int[B.length][MAX_B + 1];
+		this.n = B.length;
+		this.D = new int[n][2];
 	}
 
-	int solve(int k, int a) {
-		int m = Integer.MIN_VALUE;
-		for (int a1 = 1; a1 <= B[k - 1]; a1++) {
-			int v = Math.abs(a - a1) + D[k - 1][a1];
-			m = Math.max(m, v);
-		}
-		return m;
+	int value(int k, int a, int ib) {
+		int b = (ib == 0) ? 1 : B[k - 1];
+		return Math.abs(a - b) + D[k - 1][ib];
+	}
+
+	int value(int k, int ia) {
+		int a = (ia == 0) ? 1 : B[k];
+		return Math.max(value(k, a, MIN), value(k, a, MAX));
 	}
 
 	int solve() {
-		for (int k = 1; k < B.length; k++) {
-			for (int a = 1; a <= B[k]; a++) {
-				D[k][a] = solve(k, a);		
-			}
+		for (int k = 1; k < n; k++) {
+			D[k][MIN] = value(k, MIN);
+			D[k][MAX] = value(k, MAX);
 		}
-		int m = Integer.MIN_VALUE;
-		for (int a = 1; a <= B[B.length - 1]; a++) {
-			m = Math.max(m, D[B.length - 1][a]);
-		}
-		return m;
+		return Math.max(D[n - 1][MIN], D[n - 1][MAX]);
 	}
 
 	static SherlockAndCost load(Scanner scanner) {
