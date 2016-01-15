6118c9bb7bc1cd534decc661f32251158e740878
Damir Cohadarevic <damir.cohadarevic@sbgenomics.com>
2016-01-15 10:08:13 +0100

    not a dp

--- algorithms/dynamic-programming/TheLongestIncSubseq/TheLongestIncSubseq.java
+++ algorithms/dynamic-programming/TheLongestIncSubseq/TheLongestIncSubseq.java
@@ -4,33 +4,46 @@ import java.io.*;
 /* Mighty Cohadar */
 public class TheLongestIncSubseq {
 
+	static int findHigher(int[] A, int[] H, int l, int r, int v) {
+		for (int i = r; i >= l; i--) {
+			if (A[H[i]] < v) {
+				return i + 1;
+			}
+		}
+		return 1;
+	}
+
 	public static void main(String[] args) {
 		Scanner scanner = new Scanner(System.in);
 		int n = scanner.nextInt();
 		assert 1 <= n && n <= 1e6 : "out of range, n: " + n;
 		int[] A = scanArray(scanner, n);
-		int[] L = new int[1 + n];
-		Arrays.fill(L, 1);
-		for (int i = 0; i < n; i++) {
-			for (int j = i - 1; j >= 0; j--) {
-				if (A[i] >= A[j]) {
-					if (L[i] < L[j] + 1) {
-						L[i] = L[j] + 1;
-					}
-				}
-			}
-		}
-		System.out.println(max(L));
-	}
-
-	static int max(int[] A) {
-		int m = A[0];
+		int[] P = new int[n]; // previous indices
+		int[] H = new int[n]; // head indices
+		P[0] = -1;
+		H[0] = 0;
+		int nh = 1; 
 		for (int i = 1; i < A.length; i++) {
-			if (m < A[i]) {
-				m = A[i];
+			if (A[i] > A[H[nh - 1]]) {
+				// A[i] is biggest
+				P[i] = H[nh - 1];
+				H[nh++] = i;
+			} else if (A[i] <= A[H[0]]) {
+				// A[i] is smallest
+				P[i] = -1;
+				H[0] = i;
+			} else {
+				// A[i] is in the mid
+				int higher = findHigher(A, H, 0, nh - 1, A[i]);
+				P[i] = H[higher - 1];
+				H[higher] = i;
 			}
 		}
-		return m;
+		int count = 0;
+		for (int i = H[nh - 1]; i >= 0; i = P[i]) {
+			count++;
+		}
+		System.out.println(count);
 	}
 
 	static int[] scanArray(Scanner scanner, int n) {
