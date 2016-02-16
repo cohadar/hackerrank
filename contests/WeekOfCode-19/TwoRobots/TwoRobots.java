0e896a0f2cf4eaa5b1f16dfe00844c235b76df48
Damir Cohadarevic <damir.cohadarevic@sbgenomics.com>
2016-02-16 21:14:34 +0100

    LD examp pass

--- contests/WeekOfCode-19/TwoRobots/TwoRobots.java
+++ contests/WeekOfCode-19/TwoRobots/TwoRobots.java
@@ -12,6 +12,7 @@ public class TwoRobots {
 	final int n;
 	final int[] A;
 	final int[] B;
+	final int[][] L;
 	final int[][] D;
 	
 	public TwoRobots(int m, int n, int[] A, int[] B) {
@@ -19,41 +20,58 @@ public class TwoRobots {
 		this.n = n;
 		this.A = A;
 		this.B = B;
+		this.L = new int[n][m];
 		this.D = new int[n][m];
-	}
-	
-	static int min(int[] A) {
-		int m = INF;
-		for (int i = 0; i < A.length; i++) {
-			m = Math.min(m, A[i]);
-		}
-		return m;
 	}
 
 	public void initD() {
 		for (int k = 0; k < n; k++) {
-			Arrays.fill(D[k], INF);
+			for (int i = 0; i < m; i++) {
+				L[k][i] = INF;
+				D[k][i] = INF;
+			}
 		}
+	}
+
+	public int min() {
+		int min = INF;
+		for (int i = 0; i < m; i++) {
+			min = Math.min(min, L[n-1][i]);
+			min = Math.min(min, D[n-1][i]);
+		}
+		return min;
+	}
+
+	public void solve0() {
 		int k = 0;
+		for (int i = 0; i < m; i++) {
+			L[k][i] = Math.min(L[k][i], Math.abs(A[k] - B[k]));
+			D[k][i] = Math.min(D[k][i], Math.abs(A[k] - B[k]));
+		}
+	}
+
+	public void solveL(int k, int l) {
 		for (int r = 0; r < m; r++) {
-			D[k][r] = Math.abs(A[0] - B[0]);
+			L[k][r] = Math.min(L[k][r], L[k-1][r] + Math.abs(l - A[k]) + Math.abs(A[k] - B[k]));
+			D[k][l] = Math.min(D[k][l], L[k-1][r] + Math.abs(r - A[k]) + Math.abs(A[k] - B[k]));
+		}
+	}
+
+	public void solveR(int k, int r) {
+		for (int l = 0; l < m; l++) {
+			L[k][r] = Math.min(L[k][r], D[k-1][l] + Math.abs(l - A[k]) + Math.abs(A[k] - B[k]));
+			D[k][l] = Math.min(D[k][l], D[k-1][l] + Math.abs(r - A[k]) + Math.abs(A[k] - B[k]));
 		}
 	}
 
 	public int solve() {
-		assert n > 0;
 		initD();
+		solve0();
 		for (int k = 1; k < n; k++) {
-			for (int r = 0; r < m; r++) {
-				int val = D[k - 1][r] + Math.abs(B[k - 1] - A[k]) + Math.abs(A[k] - B[k]);
-				D[k][r] = Math.min(D[k][r], val);
-			}
-			for (int l = 0; l < m; l++) {
-				int val = D[k - 1][l] + Math.abs(l - A[k]) + Math.abs(A[k] - B[k]);
-				D[k][B[k - 1]] = Math.min(D[k][B[k - 1]], val);
-			}
+			solveL(k, B[k - 1]);
+			solveR(k, B[k - 1]);
 		}
-		return min(D[n - 1]);
+		return min();
 	}
 
 	public static TwoRobots load(Scanner scanner) {
@@ -61,8 +79,8 @@ public class TwoRobots {
 		int n = scanner.nextInt();
 		assert 1 <= m && m <= 1000 : "out of range, m: " + m;
 		assert 1 <= n && n <= 1000 : "out of range, n: " + n;
-		int[] A = new int[m];
-		int[] B = new int[m];
+		int[] A = new int[n];
+		int[] B = new int[n];
 		for (int i = 0; i < n; i++) {
 			A[i] = scanner.nextInt() - 1;
 			B[i] = scanner.nextInt() - 1;
