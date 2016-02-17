e52bbb0e1acc98ae1088ae37f032daa8a0f1951b
Damir Cohadarevic <damir.cohadarevic@sbgenomics.com>
2016-02-17 20:14:17 +0100

    static generators

--- contests/WeekOfCode-19/ScalarProducts/ScalarProducts.java
+++ contests/WeekOfCode-19/ScalarProducts/ScalarProducts.java
@@ -4,24 +4,50 @@ import java.io.*;
 /* Mighty Cohadar */
 public class ScalarProducts {
 
-	static class Generator {
-		private int a0;
-		private int a1;
-		private final int m;
-		public Generator(int a0, int a1, int m) {
-			this.a0 = a0;
-			this.a1 = a1;
-			this.m = m;
+	final int m;
+	final int n;
+	int a1, a0;
+	int b1, b0;
+	
+	public ScalarProducts(int c, int m, int n) {
+		this.m = m;
+		this.n = n;
+		this.a0 = 0;
+		this.a1 = c;
+	}
+
+	public int nextA() {
+		int a2 = (a1 + a0) % m;
+		a0 = a1;
+		a1 = a2;
+		return a2;
+	}
+
+	public int nextB() {
+		int b2 = (b1 + b0) % m;
+		b0 = b1;
+		b1 = b2;
+		return b2;
+	}
+
+	public void a2b() {
+		b1 = a1;
+		b0 = a0;
+	}
+	
+	public int solve() {
+		BitSet B = new BitSet(m);
+		for (int i = 0; i < n; i++) {
+			int x1 = nextA();
+			int y1 = nextA();
+			a2b();
+			for (int j = i + 1; j < n; j++) {
+				int x2 = nextB();
+				int y2 = nextB();	
+				B.set((x1 * x2 + y1 * y2) % m);
+			}
 		}
-		public int next() {
-			int a2 = (a1 + a0) % m;
-			a0 = a1;
-			a1 = a2;
-			return a2;
-		}
-		public Generator clone() {
-			return new Generator(this.a0, this.a1, this.m);
-		}
+		return B.cardinality() % m;
 	}
 
 	public static void main(String[] args) {
@@ -32,19 +58,8 @@ public class ScalarProducts {
 		assert 1 <= c && c <= 1e9 : "out of range, c: " + c;
 		assert 1 <= m && m <= 1e9 : "out of range, m: " + m;
 		assert 1 <= n && n <= 3e5 : "out of range, n: " + n;
-		BitSet B = new BitSet(m);
-		Generator ig = new Generator(0, c, m);
-		for (int i = 0; i < n; i++) {
-			int x1 = ig.next();
-			int y1 = ig.next();
-			Generator jg = ig.clone();
-			for (int j = i + 1; j < n; j++) {
-				int x2 = jg.next();
-				int y2 = jg.next();	
-				B.set((x1 * x2 + y1 * y2) % m);
-			}
-		}
-		System.out.println(B.cardinality() % m);
+		ScalarProducts o = new ScalarProducts(c, m, n);
+		System.out.println(o.solve());
 	}
 
 }
