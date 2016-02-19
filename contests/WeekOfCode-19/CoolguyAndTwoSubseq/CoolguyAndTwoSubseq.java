13fdd0059f1b2fe226fef8524d1d0f1ed18bf453
Damir Cohadarevic <damir.cohadarevic@sbgenomics.com>
2016-02-19 07:42:39 +0100

    brute test

--- contests/WeekOfCode-19/CoolguyAndTwoSubseq/CoolguyAndTwoSubseq.java
+++ contests/WeekOfCode-19/CoolguyAndTwoSubseq/CoolguyAndTwoSubseq.java
@@ -14,7 +14,7 @@ public class CoolguyAndTwoSubseq {
 		this.A = A;
 	}
 	
-	public int solve() {
+	public int solve2() {
 		int ans = 0;
 		for (int a = 0; a < n; a++) {
 			int l = A[a];
@@ -33,6 +33,31 @@ public class CoolguyAndTwoSubseq {
 		return ans;		
 	}
 
+	public Map<Integer, Integer> left(int b) {
+		Map<Integer, Integer> L = new TreeMap<>();
+		return L;
+	}
+
+	public Map<Integer, Integer> right(int b) {
+		Map<Integer, Integer> R = new TreeMap<>();
+		return R;
+	}
+
+	public int mul(Map<Integer, Integer> L, Map<Integer, Integer> R) {
+		return -1;
+	}
+
+	public int solve() {
+		int ans = 0;
+		for (int b = 0; b < n-2; b++) {
+			Map<Integer, Integer> L = left(b);
+			Map<Integer, Integer> R = right(b);
+			ans += mul(L, R);
+			ans %= PRIME;
+		}
+		return ans;
+	}
+
 	public static CoolguyAndTwoSubseq load(Scanner scanner) {
 		int n = scanner.nextInt();
 		assert 1 <= n && n <= 2e5 : "out of range, n: " + n;
@@ -41,9 +66,23 @@ public class CoolguyAndTwoSubseq {
 	}
 
 	public static void main(String[] args) {
+		test();
 		Scanner scanner = new Scanner(System.in);
 		CoolguyAndTwoSubseq o = CoolguyAndTwoSubseq.load(scanner);
 		System.out.println(o.solve());
+	}
+
+	// makes sure new version works as brute for small n
+	public static void test() {
+		Random random = new Random();
+		for (int n = 1; n < 10; n++) {
+			int[] A = new int[n];
+			for (int i = 0; i < A.length; i++) {
+				A[i] = 1 + random.nextInt((int)1e9);
+			}
+			CoolguyAndTwoSubseq o = new CoolguyAndTwoSubseq(n, A);
+			assert o.solve() == o.solve2() : "o.solve()=" + o.solve() + ", o.solve2()=" + o.solve2();
+		}
 	}
 
 	static int[] scanArray(Scanner scanner, int n) {
